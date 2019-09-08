9ce4ef704fa16891038c257a1a1f5a554b0e85f3
Tatyana563 <nikolaev.english@gmail.com>
2019-09-08 15:33:55 +0300

    commit8.9.2019_4

--- src/chapter15/Dog.java
+++ src/chapter15/Dog.java
@@ -4,6 +4,8 @@ public class Dog extends Pet {
     public Dog(String name) {
         super(name);
         System.out.println("testing braches");
+        System.out.println("testing conflict");
+        System.out.println("testing conflict22");
     }
 
 
