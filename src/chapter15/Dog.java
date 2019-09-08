4bb76fe63ea72033069b85d70f7c805a9c4a381a
Tatyana563 <nikolaev.english@gmail.com>
2019-09-08 15:41:57 +0300

    commit8.9.2019_5

--- src/chapter15/Dog.java
+++ src/chapter15/Dog.java
@@ -3,10 +3,7 @@ package chapter15;
 public class Dog extends Pet {
     public Dog(String name) {
         super(name);
-        System.out.println("testing braches");
-        System.out.println("testing conflict");
-        System.out.println("testing conflict22");
-        System.out.println("555555");
+        System.out.println("change in a master branch");
     }
 
 
